db1a6a316bd05b7a4c79472b482eefe99fe888d2
yyokoyama0416-cyber <y.yokoyama0416@gmail.com>
2026-02-05 09:07:40 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import time
+import requests
+import smtplib
+import os
+from bs4 import BeautifulSoup
+from email.mime.text import MIMEText
+
+# ===== メール設定（環境変数から取得）=====
+GMAIL = os.environ["GMAIL"]
+APP_PASS = os.environ["APP_PASS"]
+TO_MAIL = os.environ["TO_MAIL"]
+
+# ===== Web設定 =====
+URL = "https://jp.investing.com/currencies/usd-jpy-technical"
+HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+wanted_signals = ["強い買い", "買い", "中立", "売り", "強い売り"]
+
+# ===== 環境設定 =====
+CHECK_INTERVAL = 120
+last_summary = None
+
+
+# ===== サマリ取得 =====
+def get_summary():
+    results = {}
+
+    r = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(r.text, "lxml")
+    buttons = soup.find_all("button", attrs={"data-test": True})
+
+    for btn in buttons:
+        spans = btn.find_all("span")
+
+        if len(spans) >= 2:
+            timeframe = spans[0].get_text(strip=True)
+            signal = spans[1].get_text(strip=True)
+            results[timeframe] = signal
+
+    filtered_results = {k: v for k, v in results.items() if v in wanted_signals}
+    return filtered_results
+
+# ===== メール送信 =====
+def send_mail(text):
+    msg = MIMEText(text)
+    msg["Subject"] = "USDJPYテクニカル変化"
+    msg["From"] = GMAIL
+    msg["To"] = TO_MAIL
+
+    with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
+        smtp.login(GMAIL,APP_PASS)
+        smtp.send_message(msg)
+
+# ===== メール整形 =====
+def format_summary_email(last, current):
+    lines = []
+    for k, new in current.items():
+        old = last.get(k, "新規")
+        if old != new:
+            lines.append(f"{k}: {old} → {new}")
+        else:
+            lines.append(f"{k}: {new}")
+    return "\n".join(lines)
+
+
+# ===== 実行 =====
+print("監視開始")
+
+while True:
+    try:
+        current = get_summary()
+        print("現在:", current)
+
+        if last_summary and current != last_summary:
+            email_body = format_summary_email(last_summary, current)
+            send_mail(email_body)
+
+        last_summary = current
+
+    except Exception as e:
+        print("エラー:", e)
+
+    time.sleep(CHECK_INTERVAL)
